405be8dbebfe683c1536a076a986af62c1b6070d
gsvn1221@163.com <s961221>
2021-02-09 17:25:23 +0800

    加油！

--- src/qianfeng/class04.java
+++ src/qianfeng/class04.java
@@ -1,4 +1,34 @@
 package qianfeng;
 
+import java.util.Scanner;
+
+/*
+你是我触碰不到的风
+醒不来的梦
+ */
 public class class04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        String str = scan.next();
+//        String str2 = new String();
+//        if (str == "你是我触碰不到的风"){
+//            str2 = "醒不来的梦";
+//        }
+//        System.out.println(str2);
+//        int i1 = scan.nextInt();
+//        if(i1 == 1){
+//            System.out.println(i1);
+//        }
+        /*
+        格式化输出 printf
+        % 占位符  后面传入对应位置的参数
+         */
+        int age;
+        String name;
+        System.out.println("请输入年龄：");
+        age = scan.nextInt();
+        System.out.println("请输入姓名：");
+        name = scan.next();
+        System.out.printf("姓名：%s,年龄：%d",name,age);
+    }
 }
---
+++ src/qianfeng/class05.java
@@ -0,0 +1,20 @@
+package qianfeng;
+
+public class class05 {
+    public static void main(String[] args) {
+        /*
+        switch case when
+         */
+        int week = 5;
+        switch (week){
+            case 1:
+                System.out.println("周一");
+                break;
+            case 2:
+                System.out.println("周二");
+                break;
+            default:
+                System.out.println("输入有误！");
+        }
+    }
+}
